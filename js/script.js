851d662f094ed2fe05d56a3204bd8ecb4a33a07b
Nylah Godoy <ntng08@gmail.com>
2019-04-09 21:58:46 +0000

    giphy3

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var input = $("input").val();
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q="+ input +"&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+            var url = response.data[0].images.original.url;
+            $('div').append('<img src=' + url + '>')
+        }
+    });
 });
-
